4110cb40d8280760758fb04712c6c7ee2b021d43
GianmarcoRusso <gianmarco.russo1995@gmail.com>
2018-06-19 14:51:33 +0200

    fix lastfmWrapper

--- src/musicthieves/wrapper/LastFMWrapper.java
+++ src/musicthieves/wrapper/LastFMWrapper.java
@@ -10,19 +10,18 @@ public class LastFMWrapper {
 
 	private static final String API_KEY = "963f68cad2c206d11fbdf91543417a09"; 
 	public LastFMWrapper() {
-		
+
 	}
-	
+
 	public String[] getSong(String input) throws IOException {
-		
+		String[] results = new String[5];
+
 		Document doc = Jsoup.connect("https://ws.audioscrobbler.com/2.0/?method=track.search&track="+
-		URLEncoder.encode(input, "UTF-8")+"&api_key="	+ API_KEY).get();
-		
-		String title = doc.getElementsByTag("track").first().getElementsByTag("name").first().text();
-		String artist = doc.getElementsByTag("track").first().getElementsByTag("artist").first().text();
-		
-		String[] result = {title,artist}; 
-		return result;
+				URLEncoder.encode(input, "UTF-8")+"&api_key="	+ API_KEY).get();
+		for(int i = 0; i < 5; i++) {
+			results[i] = doc.getElementsByTag("track").get(i).getElementsByTag("name").first().text() +", "+ doc.getElementsByTag("track").get(i).getElementsByTag("artist").first().text();
+		}
+		return results;
 	}
 
 }
--- src/musicthieves/wrapper/WrapperTester.java
+++ src/musicthieves/wrapper/WrapperTester.java
@@ -4,46 +4,50 @@ import java.io.IOException;
 
 public class WrapperTester {
 
-public static void main(String[] args) throws IOException {
-		
-	/* Begin Test LyricsWrapper */
+	public static void main(String[] args) throws IOException {
+
+		/* Begin Test LyricsWrapper */
+		System.out.println("TEST LYRICS\n");
 		//search in lyric
 		String input = "partono plotoni di uomini";
 		LyricsWrapper lWrapper = new LyricsWrapper();
 		String[] data = lWrapper.search(input);
-		
+
 		for(int i = 0; i < 4; i++) {
 			System.out.println(data[i]+"");
 		}
-		
+
 		String lyric = lWrapper.getLyric(data[4]);		
-		
+
 		System.out.println(lyric+"\n");
-		
+
 		//search album
 		String artist = lWrapper.searchAlbum("Master of puppets");
 		System.out.println("L'artista dell'album cercato è: "+artist+"\n");
-	/* End Test LyricsWrapper */
-		
-	/* Begin Test LastFMWrapper */
-		String input1 = "follie preferenziali";
+		/* End Test LyricsWrapper */
+
+		System.out.println("TEST LASTFM\n");
+		/* Begin Test LastFMWrapper */
+		String input1 = "follie";
 		LastFMWrapper LFMWrapper = new LastFMWrapper();
 		String[] songInfo = LFMWrapper.getSong(input1);
+		System.out.println("Risultati per '"+input1+"': ");
+		for(int i = 0; i < 5; i++) {
+			System.out.println(songInfo[i]+"");
+		}
+		/* End Test LastFMWrapper */
 		
-		System.out.println(songInfo[0]+"\n"+songInfo[1]+"\n");
-	/* End Test LastFMWrapper */
-		
-	/* Begin Test LastFMWrapper */
+		/* Begin Test MusicMap */
+		System.out.println("\nTEST MUSICMAP\n");
 		String input2 = "Dream Theater";
 		MusicMapWrapper mapWrapper = new MusicMapWrapper();
 		String[] relatedArtist = mapWrapper.relatedArtists(input2);
-		
+
 		System.out.println("Hai cercato "+input2+". Potrebbero piacerti:");
 		for(int i = 0; i < 5; i++) {
 			System.out.println(relatedArtist[i]);
 		}
-	
-	/* End Test LastFMWrapper */
+		/* End Test MusicMap */
 	}
 
 }
