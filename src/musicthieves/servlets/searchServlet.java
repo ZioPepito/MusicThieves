1ea3c145c9f1a5e2566ae517c613abcb6be12eb9
VincenzoConte <vincenzoconte@hotmail.com>
2018-06-22 20:07:59 +0200

    youtube video

--- src/musicthieves/servlets/searchServlet.java
+++ src/musicthieves/servlets/searchServlet.java
@@ -41,6 +41,7 @@ public class searchServlet extends HttpServlet {
 		if(input!=null && input.trim()!="") {
 			if(option == 0) {
 				Song song = Mediator.searchByTitle(input);
+				String videoID=Mediator.findOnYoutube(song.getTitle());
 				response.getWriter().append("<div class=\"row\">\r\n" + 
 						"<div class=\"col-md-6\">"+
 							"<h2>Song info</h2></br>" +
@@ -66,7 +67,50 @@ public class searchServlet extends HttpServlet {
 							"<h2>Audio</h2></br>" +
 						" </div>\r\n" + 
 						"<div class=\"col-md-6\">" +
-							"<h2>Video</h2></br>" + 
+							"<h2>Video</h2></br>" +
+						"<script>"+
+						"// 2. This code loads the IFrame Player API code asynchronously.\n" + 
+						"var tag = document.createElement('script');\n" + 
+						"\n" + 
+						"tag.src = \"https://www.youtube.com/iframe_api\";\n" + 
+						"var firstScriptTag = document.getElementsByTagName('script')[0];\n" + 
+						"firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);\n" + 
+						"\n" + 
+						"// 3. This function creates an <iframe> (and YouTube player)\n" + 
+						"//    after the API code downloads.\n" + 
+						"var player;\n" + 
+						"function onYouTubeIframeAPIReady() {\n" + 
+						"	player = new YT.Player('player', {\n" + 
+						"		height : '280',\n" + 
+						"		width : '500',\n" + 
+						"		videoId : '"+videoID+"',\n" + 
+						"		events : {\n" + 
+						"			'onReady' : onPlayerReady,\n" + 
+						"			'onStateChange' : onPlayerStateChange\n" + 
+						"		}\n" + 
+						"	});\n" + 
+						"}\n" + 
+						"\n" + 
+						"// 4. The API will call this function when the video player is ready.\n" + 
+						"function onPlayerReady(event) {\n" + 
+						"	//event.target.playVideo();\n" + 
+						"}\n" + 
+						"\n" + 
+						"// 5. The API calls this function when the player's state changes.\n" + 
+						"//    The function indicates that when playing a video (state=1),\n" + 
+						"//    the player should play for six seconds and then stop.\n" + 
+						"var done = false;\n" + 
+						"function onPlayerStateChange(event) {\n" + 
+						"	if (event.data == YT.PlayerState.PLAYING && !done) {\n" + 
+						"		//setTimeout(stopVideo, 0);\n" + 
+						"		done = true;\n" + 
+						"	}\n" + 
+						"}\n" + 
+						"function stopVideo() {\n" + 
+						"	player.stopVideo();\n" + 
+						"}"+
+						"</script>"+
+						"<div id=\"player\"></div>"+
 						"</div>\r\n" + 
 						"</div>"); 
 			}else if (option == 1) {
